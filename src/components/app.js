365705e982bd18a3fca97fe80f16406f6cc92e80
Kevin Nguyen <kngwin9@gmail.com>
2017-04-09 16:28:07 -0700

    stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import styles from './app.css';
-import logo from './imgs/logo.svg';
 
 const App = () => (
-    <div>
-        <div className={styles.app}>
-            <img src={logo} className={styles.rotate}/>
-            <h1>Welcome to React!</h1>
-        </div>
-    </div>
+
+    <div>Hola World!</div>
+
 );
+
 
 export default App;
